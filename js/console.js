059a66b27c52485da33d5745bcac4b2efb1031c6
Ihor Statsenko <statsenkoin@gmail.com>
2022-10-20 20:45:13 +0300

    adds console.html

---
+++ js/console.js
@@ -0,0 +1,27 @@
+// TO USE OPEN console.html AND START LIVE SERVER
+
+'use strict';
+
+console.log('Hello, Console!');
+
+// ----- alert() ---------------------
+
+// Виводить модальне вікно-повідомлення
+const message = 'JavaScript is awesome!';
+alert(message);
+
+// ----- confirm() ------------------
+
+// Просимо клієнта підтвердити бронювання готелю
+// і зберігаємо результат роботи confirm у змінну
+// OK повертає true, Cancel - false
+// const isComing = confirm('Please confirm hotel reservation');
+// console.log(isComing);
+
+// ----- prompt() --------------------
+
+// Запитуємо назву готеля, в якому хотів би зупинитися клієнт
+// і зберігаємо результат виклику prompt у змінну.
+// OK повертає рядок - те, що ввів користувач, Cancel - null
+// const hotelName = prompt('Please enter desired hotel name');
+// console.log(hotelName);
