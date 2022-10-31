a348ea3c3fb5f1a4444d85a94ad66e1652071e88
Ihor Statsenko <statsenkoin@gmail.com>
2022-10-31 16:57:18 +0200

    Update tasks.js

--- js/module-02/lections/tasks.js
+++ js/module-02/lections/tasks.js
@@ -137,7 +137,13 @@ js const numbers = [2, 17, 94, 1, 23, 37];
 */
 
 const numbers = [2, 17, 94, 1, 23, 37];
-let min;
+let min = numbers[0];
+// for (i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] < min) min = numbers[i];
+// }
+for (const number of numbers) {
+  if (number < min) min = number;
+}
 console.log(min); // 1
 
 /** ============================================================================
