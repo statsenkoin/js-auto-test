fa8f215aa4d06ef62e009a7010dcd0bad012ae7b
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-04 19:08:29 +0200

    Update auto-test-01-10.js

--- js/module-04/auto-test/auto-test-01-10.js
+++ js/module-04/auto-test/auto-test-01-10.js
@@ -556,17 +556,31 @@ numbers.forEach(logMessage);
 forEach(), на стрілочну функцію.
 */
 
-// Change code below this line
-function calculateTotalPrice(orderedItems) {
-  let totalPrice = 0;
+// // Change code below this line
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
 
-  orderedItems.forEach(function (item) {
-    totalPrice += item;
-  });
+//   orderedItems.forEach(function (item) {
+//     totalPrice += item;
+//   });
 
-  return totalPrice;
-}
-// Change code above this line
+//   return totalPrice;
+// }
+
+// const calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach((item) => (totalPrice += item));
+
+//   return totalPrice;
+// };
+
+// // Change code above this line
+
+// // ----------------------------------------------------------------------------
+// myLog(calculateTotalPrice([12, 85, 37, 4])); //138
+// myLog(calculateTotalPrice([164, 48, 291])); //503
+// myLog(calculateTotalPrice([412, 371, 94, 63, 176])); //1116
 
 // ============================================================================
 
