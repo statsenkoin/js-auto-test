f96112793f7349c3caaa11de70a76cfde3c8116f
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-04 18:27:57 +0200

    Update auto-test-01-10.js

--- js/module-04/auto-test/auto-test-01-10.js
+++ js/module-04/auto-test/auto-test-01-10.js
@@ -263,4 +263,62 @@ processCall("Poly", takeCall, leaveHoloMessage);
 
 // =======================================================================
 
-/** 5 */
+/** 5 МЕТОД FOREACH(CALLBACK)
+Метод перебирання масиву, який використовується для заміни циклів for і 
+for...of в роботі з колекцією.
+
+массив.forEach(function callback(element, index, array) {
+  // Тіло колбек-функції
+});
+
+Поелементо перебирає масив.
+Викликає колбек-функцію для кожного елемента масиву.
+Нічого не повертає.
+Аргументи колбек-функції - це значення поточного елемента element, 
+його індекс index і власне вихідний масив array. Можна оголошувати 
+тільки необхідні параметри, найчастіше - це елемент, головне не забувати 
+про їх порядок.
+
+const numbers = [5, 10, 15, 20, 25];
+
+// Класичний for
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(`Індекс ${i}, значення ${numbers[i]}`);
+}
+
+// Метод перебирання forEach
+numbers.forEach(function (number, index) {
+  console.log(`Індекс ${index}, значення ${number}`);
+});
+
+Єдиним випадком, коли варто використовувати цикли for або for...of для 
+перебирання масиву, - це задачі з перериванням виконання циклу. Перервати 
+виконання методу forEach не можна, він завжди перебирає масив до кінця.
+
+// --------------------------------------------------------------------------
+Функція calculateTotalPrice(orderedItems) приймає один параметр orderedItems - 
+масив чисел, і розраховує загальну суму його елементів, яка зберігається у 
+змінній totalPrice і повертається як результат роботи функції.
+
+Виконай рефакторинг функції таким чином, щоб замість циклу for, вона 
+використовувала метод forEach.
+*/
+
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Change code below this line
+
+  // for (let i = 0; i < orderedItems.length; i += 1) {
+  //   totalPrice += orderedItems[i];
+  // }
+
+  orderedItems.forEach((item) => (totalPrice += item));
+
+  // Change code above this line
+  return totalPrice;
+}
+
+// --------------------------------------------------------------------------
+console.log(calculateTotalPrice([12, 85, 37, 4])); //138
+console.log(calculateTotalPrice([164, 48, 291])); //503
+console.log(calculateTotalPrice([412, 371, 94, 63, 176])); //1116
