699c97dcd3f15f0273786d2a0f1a393698407dbd
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-06 19:49:56 +0200

    Update auto-test-28-40.js

--- js/module-04/auto-test/auto-test-28-40.js
+++ js/module-04/auto-test/auto-test-28-40.js
@@ -932,3 +932,251 @@ console.log(inReversedOrder);
 // myLog(authorsInReversedOrder);
 // //["Tanith Lee", "Robert Sheckley", "Howard Lovecraft",
 // // "Fyodor Dostoevsky", "Bernard Cornwell"]
+
+// ============================================================================
+
+/** 41 СОРТУВАННЯ ОБ'ЄКТІВ
+Під час роботи з масивом об'єктів, сортування виконується за числовим або 
+рядковим значенням певної властивості. Наприклад, у нас є група студентів 
+з балами за тест. Необхідно відсортувати масив об'єктів за зростанням і 
+спаданням кількості балів, і за ім'ям студента.
+
+const students = [
+  { name: "Mango", score: 83 },
+  { name: "Poly", score: 59 },
+  { name: "Ajax", score: 37 },
+  { name: "Kiwi", score: 94 },
+];
+
+const inAscendingScoreOrder = students.sort(
+  (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
+);
+
+const inDescendingScoreOrder = students.sort(
+  (firstStudent, secondStudent) => secondStudent.score - firstStudent.score
+);
+
+const inAlphabeticalOrder = students.sort((firstStudent, secondStudent) =>
+  firstStudent.name.localeCompare(secondStudent.name)
+);
+
+// ----------------------------------------------------------------------------
+Доповни код таким чином, щоб:
+
+У змінній sortedByAuthorName вийшов масив книг, відсортований за ім'ям 
+автора в алфавітному порядку.
+У змінній sortedByReversedAuthorName вийшов масив книг, відсортований 
+за ім'ям автора у зворотному алфавітному порядку.
+У змінній sortedByAscendingRating вийшов масив книг, відсортований 
+за зростанням рейтингу.
+У змінній sortedByDescentingRating вийшов масив книг, відсортований 
+за спаданням рейтингу.
+*/
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
+
+// // Change code below this line
+
+// const sortedByAuthorName = [...books].sort((a, b) =>
+//   a.author.localeCompare(b.author)
+// );
+
+// const sortedByReversedAuthorName = [...books].sort((a, b) =>
+//   b.author.localeCompare(a.author)
+// );
+
+// const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
+// const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
+
+// ----------------------------------------------------------------------------
+// myLog(sortedByAuthorName);
+// myLog(sortedByReversedAuthorName);
+// myLog(sortedByAscendingRating);
+// myLog(sortedByDescentingRating);
+
+// ============================================================================
+
+/** 42 ЗАДАЧА. СОРТУВАННЯ ЗА БАЛАНСОМ
+Доповни функцію sortByAscendingBalance(users) таким чином, щоб вона повертала 
+масив користувачів, відсортований за зростанням їх балансу (властивість balance).
+*/
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику
+// функції із завдання.
+
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+
+// // Change code below this line
+// const sortByAscendingBalance = (users) =>
+//   [...users].sort((a, b) => a.balance - b.balance);
+// // Change code above this line
+
+// // ----------------------------------------------------------------------------
+// myLog(sortByAscendingBalance(users));
+
+// ============================================================================
+
+/** 43 ЗАДАЧА. СОРТУВАННЯ ЗА КІЛЬКІСТЮ ДРУЗІВ
+Доповни функцію sortByDescendingFriendCount(users) таким чином, щоб вона 
+повертала масив користувачів, відсортований за спаданням кількості їхніх 
+друзів (властивість friends).
+*/
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику
+// функції із завдання.
+
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+  },
+];
+
+// Change code below this line
+const sortByDescendingFriendCount = (users) =>
+  [...users].sort((a, b) => b.friends.length - a.friends.length);
+// Change code above this line
+
+// // ----------------------------------------------------------------------------
+myLog(sortByDescendingFriendCount(users));
