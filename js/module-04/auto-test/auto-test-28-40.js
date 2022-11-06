2c9ea71ce006ee8ca1d189777744d90d4c47d421
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-06 17:09:15 +0200

    Update auto-test-28-40.js

--- js/module-04/auto-test/auto-test-28-40.js
+++ js/module-04/auto-test/auto-test-28-40.js
@@ -222,3 +222,153 @@ colorPickerOptions.find((option) => option.label === "white");
 // myLog(eachElementInSecondIsOdd); //true
 // myLog(eachElementInThirdIsEven); //false
 // myLog(eachElementInThirdIsOdd); //false
+
+/** 31 ЗАДАЧА. ЧИ ВСІ КОРИСТУВАЧІ АКТИВНІ
+Доповни функцію isEveryUserActive(users) таким чином, щоб вона перевіряла, чи 
+всі користувачі зараз активні (властивість isActive) і повертала true або false.
+*/
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час
+// виклику функції із завдання.
+
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+
+// // Change code below this line
+// const isEveryUserActive = (users) => users.every((user) => user.isActive);
+// // Change code above this line
+
+// // ----------------------------------------------------------------------------
+// myLog(isEveryUserActive(users)); //false
+
+// ============================================================================
+
+/** 32 МЕТОД SOME()
+Метод some(callback) перевіряє, чи проходить хоча б один елемент масиву тест 
+колбек-функції. Повертає true або false.
+
+масив.some((element, index, array) => {
+  // Callback function body
+});
+
+Не змінює оригінальний масив.
+Поелементо перебирає оригінальний масив.
+Повертає true, якщо хоча б один елемент масиву задовольняє умову.
+Повертає false, якщо жоден елемент масиву не задовольняє умову.
+Перебирання масиву припиняється, якщо колбек повертає true.
+
+// Чи є хоча б один елемент, що більший або дорівнює нулю? - так
+[1, 2, 3, 4, 5].some(value => value >= 0); // true
+
+// Чи є хоча б один елемент, що більший або дорівнює нулю? - так
+[-7, -20, 3, -10, -14].some(value => value >= 0); // true
+
+// Чи є хоча б один елемент, що менший нуля? - ні
+[1, 2, 3, 4, 5].some(value => value < 0); // false
+
+// Чи є хоча б один елемент, що менший нуля? - так
+[1, 2, 3, -10, 4, 5].some(value => value < 0); // true
+
+// ----------------------------------------------------------------------------
+Використовуючи метод some(), доповни код таким чином, щоб:
+
+У змінній anyElementInFirstIsEven був результат перевірки наявності парних 
+елементів в масиві firstArray.
+У змінній anyElementInFirstIsOdd був результат перевірки наявності непарних 
+елементів в масиві firstArray.
+У змінній anyElementInSecondIsEven був результат перевірки наявності парних 
+елементів в масиві secondArray.
+У змінній anyElementInSecondIsOdd був результат перевірки наявності непарних 
+елементів в масиві secondArray.
+У змінній anyElementInThirdIsEven був результат перевірки наявності парних 
+елементів в масиві thirdArray.
+У змінній anyElementInThirdIsOdd був результат перевірки наявності непарних 
+елементів в масиві thirdArray.
+*/
+
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change below this line
+
+// const anyElementInFirstIsEven = firstArray.some((el) => el % 2 === 0);
+// const anyElementInFirstIsOdd = firstArray.some((el) => el % 2 !== 0);
+
+// const anyElementInSecondIsEven = secondArray.some((el) => el % 2 === 0);
+// const anyElementInSecondIsOdd = secondArray.some((el) => el % 2 !== 0);
+
+// const anyElementInThirdIsEven = thirdArray.some((el) => el % 2 === 0);
+// const anyElementInThirdIsOdd = thirdArray.some((el) => el % 2 !== 0);
+
+// // ----------------------------------------------------------------------------
+// myLog(anyElementInFirstIsEven); //true
+// myLog(anyElementInFirstIsOdd); //false
+// myLog(anyElementInSecondIsEven); //false
+// myLog(anyElementInSecondIsOdd); //true
+// myLog(anyElementInThirdIsEven); //true
+// myLog(anyElementInThirdIsOdd); //true
+
+// ============================================================================
