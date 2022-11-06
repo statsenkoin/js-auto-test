926026ee355e86bf0751e91b17431f7fc73b24a2
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-06 20:30:50 +0200

    Update auto-test-28-40.js

--- js/module-04/auto-test/auto-test-28-40.js
+++ js/module-04/auto-test/auto-test-28-40.js
@@ -1377,6 +1377,195 @@ console.log(uniqueSortedCourses);
 // Цей масив об'єктів ми будемо передавати в параметр users під час виклику
 // функції із завдання.
 
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: [
+//       'Jacklyn Lucas',
+//       'Linda Chapman',
+//       'Adrian Cross',
+//       'Solomon Fokes',
+//     ],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+
+// // Change code below this line
+// const getNamesSortedByFriendCount = (users) =>
+//   [...users]
+//     .sort((a, b) => a.friends.length - b.friends.length)
+//     .map((user) => user.name);
+// // Change code above this line
+
+// // ----------------------------------------------------------------------------
+// myLog(getNamesSortedByFriendCount(users));
+// //["Moore Hensley", "Sharlene Bush", "Elma Head", "Sheree Anthony",
+// // "Ross Vazquez", "Carey Barr", "Blackburn Dotson"]
+
+// ============================================================================
+
+/** 47 ЗАДАЧА. ІМЕНА ДРУЗІВ
+Доповни функцію getSortedFriends(users) таким чином, щоб вона повертала масив 
+унікальних імен друзів (властивість friends), відсортований за алфавітом.
+*/
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику
+// функції із завдання.
+
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: [
+//       'Jacklyn Lucas',
+//       'Linda Chapman',
+//       'Adrian Cross',
+//       'Solomon Fokes',
+//     ],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+
+// // Change code below this line
+// const getSortedFriends = (users) =>
+//   [...users]
+//     .flatMap((user) => user.friends)
+//     .filter((friend, index, array) => array.indexOf(friend) === index)
+//     .sort((a, b) => a.localeCompare(b));
+
+// // Change code above this line
+// // // ----------------------------------------------------------------------------
+// myLog(getSortedFriends(users));
+//["Adrian Cross", "Aisha Tran", "Briana Decker", "Eddie Strong",
+// "Goldie Gentry", "Jacklyn Lucas", "Jordan Sampson", "Linda Chapman",
+// "Marilyn Mcintosh", "Naomi Buckner", "Padilla Garrison",
+// "Sharron Pace", "Solomon Fokes"]
+
+// ============================================================================
+
+/** 48 ЗАДАЧА. ІМЕНА ДРУЗІВ
+Доповни функцію getSortedFriends(users) таким чином, щоб вона повертала масив 
+унікальних імен друзів (властивість friends), відсортований за алфавітом.
+*/
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику
+// функції із завдання.
+
 const users = [
   {
     name: 'Moore Hensley',
@@ -1448,11 +1637,17 @@ const users = [
   },
 ];
 
-// Change code below this line
-const getNamesSortedByFriendCount = (users) => {};
-// Change code above this line
+// // Change code below this line
+// const getSortedFriends = (users) =>
+//   [...users]
+//     .flatMap((user) => user.friends)
+//     .filter((friend, index, array) => array.indexOf(friend) === index)
+//     .sort((a, b) => a.localeCompare(b));
 
-// ----------------------------------------------------------------------------
-myLog(getNamesSortedByFriendCount(users));
-//["Moore Hensley", "Sharlene Bush", "Elma Head", "Sheree Anthony",
-// "Ross Vazquez", "Carey Barr", "Blackburn Dotson"]
+// // Change code above this line
+// // // ----------------------------------------------------------------------------
+// myLog(getSortedFriends(users));
+//["Adrian Cross", "Aisha Tran", "Briana Decker", "Eddie Strong",
+// "Goldie Gentry", "Jacklyn Lucas", "Jordan Sampson", "Linda Chapman",
+// "Marilyn Mcintosh", "Naomi Buckner", "Padilla Garrison",
+// "Sharron Pace", "Solomon Fokes"]
