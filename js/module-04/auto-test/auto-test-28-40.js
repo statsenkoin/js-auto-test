3f6ed178e57ca6d853120adff7edfe468dfef383
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-06 17:30:49 +0200

    Update auto-test-28-40.js

--- js/module-04/auto-test/auto-test-28-40.js
+++ js/module-04/auto-test/auto-test-28-40.js
@@ -527,3 +527,49 @@ previousValue = 0
 
 // // ----------------------------------------------------------------------------
 // myLog(totalPlayTime); //2692
+
+// ============================================================================
+
+/** 35 МЕТОД REDUCE() І МАСИВ ОБ'ЄКТІВ
+Під час роботи з масивом об'єктів виконується редукування за значенням певної 
+властивості. Наприклад, у нас є масив студентів з балами за тест. Необхідно 
+отримати середній бал.
+
+const students = [
+  { name: "Mango", score: 83 },
+  { name: "Poly", score: 59 },
+  { name: "Ajax", score: 37 },
+  { name: "Kiwi", score: 94 },
+  { name: "Houston", score: 64 },
+];
+
+// Назва акумулятора може бути довільною, це просто параметр функції
+const totalScore = students.reduce((total, student) => {
+  return total + student.score;
+}, 0);
+
+const averageScore = totalScore / students.length;
+
+// ----------------------------------------------------------------------------
+Нашому сервісу необхідно розрахувати середній час, проведений в одній грі для 
+кожного гравця, і отримати загальну суму цих значень часу. Розрахувати час для 
+кожного з гравців можна, розділивши його час (властивість playtime) на 
+кількість ігор (властивість gamesPlayed).
+*/
+
+// const players = [
+//   { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
+//   { name: 'Poly', playtime: 469, gamesPlayed: 2 },
+//   { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
+//   { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce(
+//   (averageTime, nextPlayer) =>
+//     averageTime + nextPlayer.playtime / nextPlayer.gamesPlayed,
+//   0
+// );
+
+// // ----------------------------------------------------------------------------
+// myLog(totalAveragePlaytimePerGame); //1023
