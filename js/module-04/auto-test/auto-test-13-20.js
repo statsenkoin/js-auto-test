8e3f824201555f95180ff102428802c55b2d48e4
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-06 16:23:18 +0200

    Update auto-test-13-20.js

--- js/module-04/auto-test/auto-test-13-20.js
+++ js/module-04/auto-test/auto-test-13-20.js
@@ -879,6 +879,104 @@ console.log(average); // Масив об'єктів з іменами Poly і Ho
 // Цей масив об'єктів ми будемо передавати в параметр users під час виклику
 // функції із завдання.
 
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//     age: 37,
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//     age: 34,
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//     age: 24,
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//     age: 21,
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//     age: 27,
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//     age: 38,
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//     age: 39,
+//   },
+// ];
+
+// // Change code below this line
+// const getUsersWithFriend = (users, friendName) =>
+//   users.filter(({ friends }) => friends.includes(friendName));
+// // Change code above this line
+
+// ------------------------------------------------------------------------
+// myLog(getUsersWithFriend(users, 'Briana Decker'));
+//Sharlene Bush і Sheree Anthony
+// myLog(getUsersWithFriend(users, 'Goldie Gentry'));
+//Elma Head і Sheree Anthony
+// myLog(getUsersWithFriend(users, 'Adrian Cross'));
+//[]
+
+// ========================================================================
+
+/** 25 ЗАДАЧА. СПИСОК ДРУЗІВ
+Доповни функцію getFriends(users) таким чином, щоб вона повертала масив 
+друзів всіх користувачів (властивість friends). У декількох користувачів 
+можуть бути однакові друзі, зроби так, щоб масив, що повертається, 
+не містив повторень.
+*/
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час
+// виклику функції із завдання.
+
 const users = [
   {
     name: 'Moore Hensley',
@@ -952,15 +1050,25 @@ const users = [
   },
 ];
 
-// Change code below this line
-const getUsersWithFriend = (users, friendName) =>
-  users.filter(({ friends }) => friends.includes(friendName));
+// // Change code below this line
+// const getFriends = (users) => {
+//   const flatFriends = users.flatMap((user) => user.friends);
+//   const friendList = flatFriends.filter(
+//     (friend, index, array) => array.indexOf(friend) === index
+//   );
+//   return friendList;
+// };
 // Change code above this line
 
-// ------------------------------------------------------------------------
-// myLog(getUsersWithFriend(users, 'Briana Decker'));
-//Sharlene Bush і Sheree Anthony
-// myLog(getUsersWithFriend(users, 'Goldie Gentry'));
-//Elma Head і Sheree Anthony
-myLog(getUsersWithFriend(users, 'Adrian Cross'));
-//[]
+const getFriends = (users) => {
+  return users
+    .flatMap((user) => user.friends)
+    .filter((friend, index, array) => array.indexOf(friend) === index);
+};
+
+// -----------------------------------------------------------------------
+myLog(getFriends(users));
+// ["Sharron Pace", "Briana Decker", "Marilyn Mcintosh",
+// "Padilla Garrison", "Naomi Buckner", "Goldie Gentry",
+// "Aisha Tran", "Jordan Sampson", "Eddie Strong",
+// "Jacklyn Lucas", "Linda Chapman"]
