d65028d4b57ed190d85d47d553ff7d3c90f67c16
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-06 18:43:54 +0200

    Update auto-test-28-40.js

--- js/module-04/auto-test/auto-test-28-40.js
+++ js/module-04/auto-test/auto-test-28-40.js
@@ -573,3 +573,247 @@ const averageScore = totalScore / students.length;
 
 // // ----------------------------------------------------------------------------
 // myLog(totalAveragePlaytimePerGame); //1023
+
+// ============================================================================
+
+/** 36 ЗАДАЧА. ЗАГАЛЬНИЙ БАЛАНС КОРИСТУВАЧІВ
+Доповни функцію calculateTotalBalance(users) таким чином, щоб вона рахувала і 
+повертала суму всіх коштів (властивість balance), які зберігають користувачі 
+з масиву users.
+*/
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику
+// функції із завдання.
+
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+
+// // Change code below this line
+// const calculateTotalBalance = (users) =>
+//   users.reduce((acc, user) => acc + user.balance, 0);
+// // Change code above this line
+
+// // ----------------------------------------------------------------------------
+// myLog(calculateTotalBalance(users)); //20916
+
+// ============================================================================
+
+/** 37 ЗАДАЧА. ЗАГАЛЬНА КІЛЬКІСТЬ ДРУЗІВ
+Доповни функцію getTotalFriendCount(users) таким чином, щоб вона рахувала і 
+повертала загальну кількість друзів (властивість friends) усіх користувачів з 
+масиву users.
+*/
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику
+// функції із завдання.
+
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+
+// // Change code below this line
+// const getTotalFriendCount = (users) =>
+//   users.reduce((acc, user) => acc + user.friends.length, 0);
+// // Change code above this line
+
+// // ----------------------------------------------------------------------------
+// myLog(getTotalFriendCount(users)); //14
+
+// ============================================================================
+
+/** 38МЕТОД SORT()
+Метод sort() сортує елементи масиву, але на відміну від інших методів перебирання, 
+він сортує вихідний масив.
+
+Сортує і змінює вихідний масив.
+Повертає змінений масив, тобто посилання на відсортований вихідний.
+За замовчуванням сортує за зростанням.
+Сортування відбувається шляхом приведення значень до рядка і порівняння 
+порядкових номерів у таблиці Unicode.
+Такий масив чисел буде відсортований за зростанням.
+
+const scores = [61, 19, 74, 35, 92, 56];
+scores.sort();
+console.log(scores); // [19, 35, 56, 61, 74, 92]
+
+Але, оскільки за замовчуванням значення приводяться до рядка, стандартне 
+сортування чисел працює незвично. Тому, у наступній вправі ми розглянемо як 
+задавати свій порядок сортування.
+
+const scores = [27, 2, 41, 4, 7, 3, 75];
+scores.sort();
+console.log(scores); // [2, 27, 3, 4, 41, 7, 75]
+
+Масив рядків сортується за алфавітом.
+
+const students = ["Jacob", "Artemis", "Solomon", "Adrian", "Kai", "Ganymede"];
+students.sort();
+console.log(students); // [ "Adrian", "Artemis", "Ganymede", "Jacob", "Kai", "Solomon" ]
+
+Водночас порядковий номер великих літер менший, ніж у малих.
+
+const letters = ["b", "B", "a", "A", "c", "C"];
+letters.sort();
+console.log(letters); // ["A", "B", "C", "a", "b", "c"]
+
+Через те, що сортується вихідний масив, порушується принцип чистоти функцій і 
+не можна зручно створити декілька похідних колекцій на основі вихідної. 
+Наприклад, створити колекцію, відсортовану за зростанням, а іншу - 
+за спаданням. Тому перед сортуванням роблять повну копію вихідного масиву і 
+сортують вже її.
+
+const scores = [61, 19, 74, 35, 92, 56];
+const ascendingScores = [...scores].sort();
+
+console.log(scores); // [61, 19, 74, 35, 92, 56]
+console.log(ascendingScores); // [19, 35, 56, 61, 74, 92]
+
+// ----------------------------------------------------------------------------
+Доповни код таким чином, щоб у змінній ascendingReleaseDates вийшла копія масиву 
+releaseDates, відсортована за зростанням, а у змінній alphabeticalAuthors - 
+копія масиву імен авторів authors, відсортована за алфавітом.
+*/
+
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   'Tanith Lee',
+//   'Bernard Cornwell',
+//   'Robert Sheckley',
+//   'Fyodor Dostoevsky',
+// ];
+// // Change code below this line
+
+// const ascendingReleaseDates = [...releaseDates].sort();
+
+// const alphabeticalAuthors = [...authors].sort();
+
+// // ----------------------------------------------------------------------------
+// myLog(ascendingReleaseDates);
+// //[1967, 1973, 1984, 1997, 2008, 2012, 2016]
+// myLog(alphabeticalAuthors);
+// //['Bernard Cornwell','Fyodor Dostoevsky','Robert Sheckley','Tanith Lee']
