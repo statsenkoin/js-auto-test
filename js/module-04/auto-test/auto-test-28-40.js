0e21da6af5f0d27143b8a17a8a446b26460fa56e
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-06 20:15:31 +0200

    Update auto-test-28-40.js

--- js/module-04/auto-test/auto-test-28-40.js
+++ js/module-04/auto-test/auto-test-28-40.js
@@ -1265,3 +1265,194 @@ const inAlphabeticalOrder = students.sort((firstStudent, secondStudent) =>
 
 // // ----------------------------------------------------------------------------
 // myLog(sortByName(users));
+
+// ============================================================================
+
+/** 45 ЛАНЦЮЖКИ МЕТОДІВ (ЧЕЙНІНГ, CHAINING)
+У нас є масив об'єктів з іменами, балами і відвідуваними предметами 
+кожного студента.
+
+const students = [
+  { name: "Mango", score: 83, courses: ["mathematics", "physics"] },
+  { name: "Poly", score: 59, courses: ["science", "mathematics"] },
+  { name: "Ajax", score: 37, courses: ["physics", "biology"] },
+  { name: "Kiwi", score: 94, courses: ["literature", "science"] },
+];
+Необхідно отримати масив їхніх імен, відсортованих за зростанням балів за тест. 
+З цією метою ми відсортуємо копію масиву методом sort(), після чого методом 
+map() створимо масив значень властивості name з відсортованого масиву.
+
+const sortedByAscendingScore = [...students].sort((a, b) => a.score - b.score);
+const names = sortedByAscendingScore.map(student => student.name);
+
+console.log(names); // ["Ajax", "Poly", "Mango", "Kiwi"]
+
+Проблема в тому, що у нас з'являються проміжні змінні після кожної операції, 
+крім фінальної. Змінна sortedByAscendingScore - зайва і необхідна тільки для 
+зберігання проміжного результату.
+
+Позбутися таких «мертвих» змінних можна за допомогою групування викликів 
+методів у ланцюжки. Кожний наступний метод буде виконуватися на основі 
+результату роботи попереднього.
+
+const names = [...students]
+  .sort((a, b) => a.score - b.score)
+  .map(student => student.name);
+
+console.log(names); // ["Ajax", "Poly", "Mango", "Kiwi"]
+
+Робимо копію вихідного масиву перед сортуванням.
+На копії викликаємо метод sort().
+До результату роботи методу sort() застосовуємо метод map().
+Змінній names присвоюється результат роботи методу map().
+Отримаємо масив унікальних відвідуваних предметів, відсортований за алфавітом.
+
+const uniqueSortedCourses = students
+  .flatMap(student => student.courses)
+  .filter((course, index, array) => array.indexOf(course) === index)
+  .sort((a, b) => a.localeCompare(b));
+
+console.log(uniqueSortedCourses); 
+// ["biology", "science", "literature", "mathematics", "physics"]
+
+На вихідному масиві викликаємо flatMap() і робимо розгладжений 
+масив усіх курсів.
+До результату методу flatMap() застосовуємо метод filter() для 
+фільтрації унікальних елементів.
+На результаті методу filter() викликаємо sort().
+Змінній uniqueSortedCourses присвоюється результат роботи методу sort().
+Ланцюжок методів може бути довільної довжини, але, зазвичай, не більше 
+2-3 операцій. По-перше, перебираючі методи використовуються для порівняно 
+простих операцій над колекцією. По-друге, виклик кожного наступного 
+методу - це додаткове перебирання масиву, що за великої кількості, 
+може позначитися на продуктивності.
+
+// ----------------------------------------------------------------------------
+Доповни код таким чином, щоб у змінній names вийшов масив імен авторів в 
+алфавітному порядку, рейтинг книг яких більший за значення 
+змінної MIN_BOOK_RATING.
+*/
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   {
+//     title: 'The Dreams in the Witch House',
+//     author: 'Howard Lovecraft',
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+
+// const names = [...books]
+//   .filter((book) => book.rating > MIN_BOOK_RATING)
+//   .map((book) => book.author)
+//   .sort((a, b) => a.localeCompare(b));
+// // ----------------------------------------------------------------------------
+// myLog(names);
+
+// ============================================================================
+
+/** 46 ЗАДАЧА. КОРИСТУВАЧІ І ДРУЗІ
+Доповни функцію getNamesSortedByFriendCount(users) таким чином, щоб вона 
+повертала масив імен користувачів, відсортований за зростанням кількості їхніх 
+друзів (властивість friends).
+*/
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику
+// функції із завдання.
+
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: [
+      'Jacklyn Lucas',
+      'Linda Chapman',
+      'Adrian Cross',
+      'Solomon Fokes',
+    ],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+  },
+];
+
+// Change code below this line
+const getNamesSortedByFriendCount = (users) => {};
+// Change code above this line
+
+// ----------------------------------------------------------------------------
+myLog(getNamesSortedByFriendCount(users));
+//["Moore Hensley", "Sharlene Bush", "Elma Head", "Sheree Anthony",
+// "Ross Vazquez", "Carey Barr", "Blackburn Dotson"]
