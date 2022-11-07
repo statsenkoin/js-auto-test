f2f34ed9cd47a134630d81288b178346f18b9b6f
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-07 22:08:48 +0200

    Update codewars.js

--- js/codewars.js
+++ js/codewars.js
@@ -224,12 +224,14 @@ Examples:(Input --> Output)
 //   return Math.sqrt(sq) % 1 !== 0 ? -1 : Math.pow(Math.sqrt(sq) + 1, 2);
 // }
 
-const findNextSquare = (sq) =>
-  Math.sqrt(sq) % 1 !== 0 ? -1 : Math.pow(Math.sqrt(sq) + 1, 2);
+// const findNextSquare = (sq) =>
+//   Math.sqrt(sq) % 1 !== 0 ? -1 : Math.pow(Math.sqrt(sq) + 1, 2);
 
-// -------------------------
-console.log(findNextSquare(121)); //144
-console.log(findNextSquare(625)); //676
-console.log(findNextSquare(114)); //-1
-console.log(findNextSquare(319225)); //320356
-console.log(findNextSquare(15241383936)); //15241630849
+// // -------------------------
+// console.log(findNextSquare(121)); //144
+// console.log(findNextSquare(625)); //676
+// console.log(findNextSquare(114)); //-1
+// console.log(findNextSquare(319225)); //320356
+// console.log(findNextSquare(15241383936)); //15241630849
+
+// ============================================================================
