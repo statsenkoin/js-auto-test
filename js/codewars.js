e87a6d775c02eb4895fb3220d6bfbd1c3e4cd51c
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-09 22:41:09 +0200

    Update codewars.js

--- js/codewars.js
+++ js/codewars.js
@@ -228,8 +228,8 @@ Examples:(Input --> Output)
 //   Math.sqrt(sq) % 1 !== 0 ? -1 : Math.pow(Math.sqrt(sq) + 1, 2);
 
 // // -------------------------
-// console.log(findNextSquare(121)); //144
 // console.log(findNextSquare(625)); //676
+// console.log(findNextSquare(121)); //144
 // console.log(findNextSquare(114)); //-1
 // console.log(findNextSquare(319225)); //320356
 // console.log(findNextSquare(15241383936)); //15241630849
